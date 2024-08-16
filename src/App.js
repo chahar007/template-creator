7bd5fc81f852e5aa6d4c0bfa48b591441ac80d16
krapal07 <krapalchahar2580@gmail.com>
2024-08-16 16:46:59 +0530

    401 error handling added

--- src/App.js
+++ src/App.js
@@ -11,30 +11,6 @@ import AppRoute from './config/routes/route';
 
 function App() {
 
-  useEffect(() => {
-    let token = getParameterByName('jwt');
-    APP_CONSTANTS.token.jwt = token
-  }, []);
-
-
-  const getParameterByName = useCallback((name, url) => {
-    if (!url) {
-      url = window.location.href;
-    }
-    name = name.replace(/[\[\]]/g, "\\$&");
-    const regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)");
-    const results = regex.exec(url);
-    if (!results) {
-      return null;
-    }
-    if (!results[2]) {
-      return "";
-    }
-    return decodeURIComponent(results[2].replace(/\+/g, " "));
-  }, []);
-
-
-
   return (
     <React.Fragment>
       <Provider store={store}>
